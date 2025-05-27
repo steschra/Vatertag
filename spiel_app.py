aab9a5cd6df94709e86ffd2647e1e691d0c17264
steschra <38071589+steschra@users.noreply.github.com>
2025-05-27 14:50:01 +0200

    Update spiel_app.py

--- spiel_app.py
+++ spiel_app.py
@@ -92,7 +92,7 @@ if st.session_state.spiel_started and not st.session_state.spieler:
 if st.session_state.spiel_started and st.session_state.spieler:
     
     st.header("Rundenverwaltung")
-    st.text(f"Spiel: {st.session_state.spielname} \n Multiplikator {st.session_state.multiplikatoren}")
+    st.text(f"Spiel: {st.session_state.spielname} \n Multiplikatoren: {st.session_state.multiplikatoren}")
 
     if st.button("Neue Runde starten"):
         st.session_state.runden.append({
