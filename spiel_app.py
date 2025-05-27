0e913cf9a5741f9f21a1432c56eae0d6d84dc301
steschra <38071589+steschra@users.noreply.github.com>
2025-05-27 17:21:42 +0200

    Update spiel_app.py

--- spiel_app.py
+++ spiel_app.py
@@ -136,19 +136,39 @@ if st.session_state.spiel_started and st.session_state.spieler:
                                         value=runde["plaetze"].get(sp["name"], 1), key=platz_key)
                 runde["plaetze"][sp["name"]] = platz
 
-    # Berechnung
+    # Punkte vor aktueller Runde berechnen
+    vorherige_punkte = {sp["name"]: 20.0 for sp in st.session_state.spieler}
+    for r in st.session_state.runden[:-1]:  # alle außer der letzten Runde
+        for sp in st.session_state.spieler:
+            einsatz = r["einsaetze"].get(sp["name"], 0)
+            platz = r["plaetze"].get(sp["name"], 1)
+            multiplikator = st.session_state.multiplikatoren[platz - 1] if platz - 1 < len(st.session_state.multiplikatoren) else 0
+            vorherige_punkte[sp["name"]] += einsatz * multiplikator
+
+    # Spieler mit den wenigsten Punkten vor der letzten Runde
+    letzter_spieler = min(vorherige_punkte, key=vorherige_punkte.get) if vorherige_punkte else None
+
+    # Gesamte Berechnung mit Sonderregel für letzte Runde
     for sp in st.session_state.spieler:
         sp["einsaetze"], sp["plaetze"], sp["gewinne"] = [], [], []
-        for runde in st.session_state.runden:
-            einsatz = runde["einsaetze"].get(sp["name"], 0)
-            platz = runde["plaetze"].get(sp["name"], 1)
+        punkte = 20.0
+        for i, r in enumerate(st.session_state.runden):
+            einsatz = r["einsaetze"].get(sp["name"], 0)
+            platz = r["plaetze"].get(sp["name"], 1)
             multiplikator = st.session_state.multiplikatoren[platz - 1] if platz - 1 < len(st.session_state.multiplikatoren) else 0
+
+            if i == len(st.session_state.runden) - 1 and sp["name"] == letzter_spieler:
+                multiplikator *= 2  # Letzter bekommt doppelte Punkte
+
             gewinn = float(einsatz * multiplikator)
             sp["einsaetze"].append(einsatz)
             sp["plaetze"].append(platz)
             sp["gewinne"].append(gewinn)
-        sp["punkte"] = 20 + sum(sp["gewinne"])
+            punkte += gewinn
 
+        sp["punkte"] = round(punkte, 2)
+
+    
     # Spielstand
     st.header("Spielstand")
     daten = []
